c4e9ecfc1cceed5053a27b336e9335575b42c8a4
Gokulviper <deangokul57@gmail.com>
2022-08-14 15:40:21 +0530

    updated

--- src/LibraryManageMent/Library.java
+++ src/LibraryManageMent/Library.java
@@ -17,7 +17,99 @@ public class Library {
         addBooks();
         while (true){
             System.out.println("1)member login\n 2)library manager\n");
+            int opt=sc.nextInt();
+            switch (opt){
+                case 1:
+                {
+                    System.out.println("1)sign in\n 2)new member sign up");
+                    int option=sc.nextInt();
+                    if(option==1){
+                        System.out.println("enter your member id");
+                        int id=sc.nextInt();
+                        if (memberIdentifier.containsKey(id)){
+                            System.out.println("welcome "+memberIdentifier.get(id).Name+"\n " );
+                            Member m=memberIdentifier.get(id);
+                            System.out.println("1)take book \n2)return book");
+                            int o=sc.nextInt();
+                            if (o==1){
+                                if(m.RemainingBooks<=0) System.out.println("soory you reached the limit 5 please return some books ");
+                                else{
+                                    for(Book b:Books){
+                         System.out.println("book id"+b.id+" book author "+b.author+"  book copies-"+b.copies+"  book available or not"+b.available+"  book category "+b.category+" book rackNo "+b.rackNo);
+                                    }
+                                    System.out.println("enter book id");
+                                    int bookId=sc.nextInt();
+                                    if(bookIdentifier.containsKey(bookId)){
+                                       Book bb=bookIdentifier.get(bookId);
+                                       if (bb.available&&bb.copies>=1){
+                                           bb.copies--;
+                                           if (bb.copies==0)bb.available=false;
+                                           m.BooksTakenAndTimeRemaining.put(bookId,12);
+                                           System.out.println("book taked sucessfully please return in 15 days otherwise fine for" +
+                                                   "5 rupess per day");
+                                       }else{
+                                           System.out.println("currently the book is not available ");
+                                       }
+                                    }else{
+                                        System.out.println("enter valid book id");
+                                    }
+                                }
+                            }else if(o==2){
+                                System.out.println("enter your member id");
+                                int memberId=sc.nextInt();
+                                System.out.println("enter book id");
+                                int bookId=sc.nextInt();
+                                  if (bookIdentifier.containsKey(bookId)){
+                                      Book b=bookIdentifier.get(bookId);
+                                      b.copies++;
+                                      b.available=true;
+                                      Member mm=memberIdentifier.get(memberId);
+                                      mm.BooksTakenAndTimeRemaining.remove(bookId);
+                                      System.out.println("thank you");
+
+                                  }else{
+                                      System.out.println("enter valid book id");
+                                  }
+
+                            }else{
+                                System.out.println("enter 1 or 2");
+                            }
+                        }else{
+                            System.out.println("sorry the id is not registered please create your account");
+                        }
+                    }else if(option==2){
+                        System.out.println("welcome to our libarary");
+                        addMember();
+                    }else{
+                        System.out.println("enter valid option");
+                    }
+                }break;
+                case 2:{
+                    System.out.println("1)view members 2)view books");
+                    int o=sc.nextInt();
+                    if (o==1){
+                        for (Member m:memberIdentifier.values()){
+                            System.out.println("member id"+m.id+" member name "+m.Name);
+                            System.out.println("books taked");
+                            for (int mmm:m.BooksTakenAndTimeRemaining.keySet()){
+                                System.out.println(bookIdentifier.get(mmm).title);
+                            }
+
+
+                        }
+                    }else{
+
+                    }
+                }break;
+            }
         }
+    }
+
+    private static void addMember() {
+        System.out.println("enter your name");
+        String name=sc.next();Member m=new Member(memberId++,name,5,new HashMap<>());
+        System.out.println(m.id + "this your new id");
+        memberIdentifier.put(m.id,m);
     }
 
     private static void addBooks() {
@@ -29,8 +121,8 @@ Book b4=new Book(bookId++,1,"acword","vijay","entertainment","entertainment",6,t
     Book b6=new Book(bookId++,4,"realWord","hari","Study","study",3,true);
     Book b7=new Book(bookId++,7,"realWord","hari","Study","study",3,true);
   Book b8=new Book(bookId++,9,"realWord","hari","Study","study",3,true);
-    bookIdentifier.put(bookId,b1); bookIdentifier.put(bookId,b2); bookIdentifier.put(bookId,b3); bookIdentifier.put(bookId,b4);
-        bookIdentifier.put(bookId,b5); bookIdentifier.put(bookId,b6); bookIdentifier.put(bookId,b7); bookIdentifier.put(bookId,b8);
+    bookIdentifier.put(b1.id,b1); bookIdentifier.put(b2.id, b2); bookIdentifier.put(b3.id, b3); bookIdentifier.put(b4.id,b4);
+        bookIdentifier.put(b5.id,b5); bookIdentifier.put(b6.id,b6); bookIdentifier.put(b7.id,b7); bookIdentifier.put(b8.id,b8);
     }
 
     private static void addMembers() {
@@ -39,7 +131,7 @@ Book b4=new Book(bookId++,1,"acword","vijay","entertainment","entertainment",6,t
         Member m3=new Member(memberId++,"ajay",5,new HashMap<>());
         Member m4=new Member(memberId++,"vicky",5,new HashMap<>());
         Member m5=new Member(memberId++,"arul",5,new HashMap<>());
-        memberIdentifier.put(memberId,m1); memberIdentifier.put(memberId,m2); memberIdentifier.put(memberId,m3);
-        memberIdentifier.put(memberId,m4); memberIdentifier.put(memberId,m5);
+        memberIdentifier.put(m1.id,m1); memberIdentifier.put(m2.id, m2); memberIdentifier.put(m3.id,m3);
+        memberIdentifier.put(m4.id,m4); memberIdentifier.put(m5.id,m5);
     }
 }
